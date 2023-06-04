52555cb1fb5a3240bb3db3e12328498fc13a4045
Sevzera <svzr.47@gmail.com>
2023-06-04 13:14:34 -0300

    add frontend queries (falta testar)

---
+++ frontend/src/api/post.js
@@ -0,0 +1,57 @@
+import { useQuery } from "react-query";
+
+const usePost = (id) => {
+  return useQuery(["post", id], async () => {
+    const response = await fetch(`http://localhost:1999/post/${id}`);
+    return response.json();
+  });
+};
+
+const usePosts = (filters) => {
+  const stringifiedFilters = JSON.stringify(filters);
+  return useQuery("posts", async () => {
+    const response = await fetch(
+      "http://localhost:1999/post" +
+        (filters ? `?filters=${stringifiedFilters}` : "")
+    );
+    return response.json();
+  });
+};
+
+const useCreatePost = () => {
+  return useMutation(async (data) => {
+    const response = await fetch("http://localhost:1999/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return response.json();
+  });
+};
+
+const useUpdatePost = (id) => {
+  return useMutation(async (data) => {
+    const response = await fetch("http://localhost:1999/post/" + id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return response.json();
+  });
+};
+
+const useDeletePost = (id) => {
+  return useMutation(async () => {
+    const response = await fetch("http://localhost:1999/post" + id, {
+      method: "DELETE",
+    });
+
+    return response.json();
+  });
+};
---
+++ frontend/src/api/user.js
@@ -0,0 +1,57 @@
+import { useQuery } from "react-query";
+
+const useUser = (id) => {
+  return useQuery(["user", id], async () => {
+    const response = await fetch(`http://localhost:1999/user/${id}`);
+    return response.json();
+  });
+};
+
+const useUsers = (filters) => {
+  const stringifiedFilters = JSON.stringify(filters);
+  return useQuery("users", async () => {
+    const response = await fetch(
+      "http://localhost:1999/user" +
+        (filters ? `?filters=${stringifiedFilters}` : "")
+    );
+    return response.json();
+  });
+};
+
+const useCreateUser = () => {
+  return useMutation(async (data) => {
+    const response = await fetch("http://localhost:1999/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return response.json();
+  });
+};
+
+const useUpdateUser = (id) => {
+  return useMutation(async (data) => {
+    const response = await fetch("http://localhost:1999/user/" + id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return response.json();
+  });
+};
+
+const useDeleteUser = (id) => {
+  return useMutation(async () => {
+    const response = await fetch("http://localhost:1999/user" + id, {
+      method: "DELETE",
+    });
+
+    return response.json();
+  });
+};
