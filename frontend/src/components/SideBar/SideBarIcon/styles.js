3265fcf469f20989fbfe15a1aa2c18da3e84f122
Victor <victorlrmatos@gmail.com>
2023-06-07 11:05:31 -0300

    side bar tooltip

--- frontend/src/components/SideBar/SideBarIcon/styles.js
+++ frontend/src/components/SideBar/SideBarIcon/styles.js
@@ -1,8 +1,9 @@
 export const styles = {
   icon: `relative mx-auto mt-2 mb-2 flex cursor-pointer items-center justify-center 
-  text-lightcontrast transition duration-300 ease-in-out hover:scale-125`,
+  text-light-secondary dark:text-dark-secondary transition duration-300 ease-in-out hover:scale-125`,
 
   tooltip: `absolute left-10 w-auto min-w-max origin-left scale-0 
-  rounded-md bg-lightcontrast p-2 text-xs font-bold text-white shadow-md transition-all 
+  rounded-md bg-dark-background dark:bg-light-background p-2 text-xs font-bold 
+  text-dark-secondary dark:text-light-secondary shadow-md transition-all 
   duration-100`,
 };
