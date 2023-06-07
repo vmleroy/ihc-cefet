5ae70c962723f6ef214585bd0d54547aa78163f6
Victor Le Roy <victorlrmatos@gmail.com>
2023-06-07 10:12:05 -0300

    tailwind config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 
 const App = () => {
   const [message, setMessage] = React.useState("");
-  fetch("http://localhost:1999/")
-    .then((res) => res.json())
-    .then((res) => setMessage(res.message));
+  // fetch("http://localhost:1999/")
+  //   .then((res) => res.json())
+  //   .then((res) => setMessage(res.message));
 
-  return <div className="bg-black font-bold text-red-800">{message}</div>;
+  return (
+    <div className="bg-black font-bold text-red-800">{message}
+      <div className="bg-black font-archivoNarrow text-light-primary">Hello World</div>
+    </div>
+  );
 };
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        archivoNarrow: ["Archivo Narrow"],
+        balooChettan: ["Baloo Chettan"],
+      },
+      colors: {
+        light: {
+          background: "#FFFFFF",
+          primary: "#2B8CE5",
+          secondary: "#565656",
+          inputFill: "#E7E7E7"
+        },
+        dark: {
+          background: "#3A3A3A",
+          primary: "#2B8CE5",
+          secondary: "#DADADA",
+          inputFill: "#FFFFFF"
+        },
+        inputStroke: "#3A3A3A",
+        inputIcon: "#7E7E7E",
+        inputText: "#9E9E9E",
+        success: "#00C851",
+        info: "#2B8CE5",
+        warn: "#FFBB33",
+        error: "#FE4E4E"
+      },
+    },
   },
   plugins: [],
 };
