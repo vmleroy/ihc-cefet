65b007209207f57c2743cbff41e4f518f25b43cf
Julio Cesar Rocha <julio.1009@hotmail.com>
2023-06-13 20:17:48 -0300

    [+] (Profile picture + banner image) upload with imgBB API to update user info method.

--- backend/src/User/userService.js
+++ backend/src/User/userService.js
@@ -11,6 +11,7 @@ userService.show = async (id) => {
     console.log("Error in userService.show: ", error);
   }
 };
+
 userService.index = async (filters) => {
   try {
     return await userCollection.find(filters ? { ...filters } : {}).toArray();
@@ -40,20 +41,20 @@ userService.create = async (data) => {
       updatedAt: Date.now(),
     };
 
-    if(data.profilePictureSrc == undefined){
+    if (data.profilePictureSrc == undefined) {
       return userCollection.insertOne(user)
     } else {
-    const imgbbOptions = {
-      apiKey: "d800fef0297081cd154ac0a53179efe1",
-      imagePath: data.profilePictureSrc,
-      name: data._id + Date.now(),
-    }
+      const imgbbOptions = {
+        apiKey: "d800fef0297081cd154ac0a53179efe1",
+        imagePath: data.profilePictureSrc,
+        name: data._id + Date.now(),
+      }
 
-    return imgbbUploader(imgbbOptions).then(async (response) => {
-      user.profilePictureSrc = response.url;
-      return userCollection.insertOne(user)
-    }).catch((error) => console.error(error))
-  }
+      return imgbbUploader(imgbbOptions).then(async (response) => {
+        user.profilePictureSrc = response.url;
+        return userCollection.insertOne(user)
+      }).catch((error) => console.error(error))
+    }
   } catch (error) {
     console.log("Error in userService.create: ", error);
   }
@@ -62,32 +63,43 @@ userService.create = async (data) => {
 userService.update = async (id, data) => {
   try {
     if (!id) throw new Error("Missing id");
-    const {
-      email,
-      password,
-      name,
-      profilePictureSrc,
-      bannerImageSrc,
-      friends,
-      friendRequests,
-    } = data;
 
-    const user = {
-      ...(email && { email }),
-      ...(password && { password }),
-      ...(name && { name }),
-      ...(profilePictureSrc && { profilePictureSrc }),
-      ...(bannerImageSrc && { bannerImageSrc }),
-      ...(friends && { friends }),
-      ...(friendRequests && { friendRequests }),
-      updatedAt: Date.now(),
-    };
+    const currentUserData = await userCollection.findOne({ "_id": id })
+    let newProfilePicture = currentUserData.profilePictureSrc
+    let newBanner = currentUserData.bannerImageSrc
 
-    return await userCollection.updateOne({ _id: id }, { $set: user });
+    if (data.profilePictureSrc != currentUserData.profilePictureSrc && data.profilePictureSrc != undefined) {
+      const imgbbOptions = {
+        apiKey: "d800fef0297081cd154ac0a53179efe1",
+        imagePath: data.profilePictureSrc,
+        name: currentUserData._id + Date.now(),
+      }
+      newProfilePicture = await imgbbUploader(imgbbOptions).then(async (response) => {
+        return response.url
+      }).catch((error) => console.error(error))
+    }
+
+    if (data.bannerImageSrc != currentUserData.bannerImageSrc && data.bannerImageSrc != undefined) {
+      const imgbbOptions = {
+        apiKey: "d800fef0297081cd154ac0a53179efe1",
+        imagePath: data.bannerImageSrc,
+        name: "banner_" + currentUserData._id + Date.now(),
+      }
+      newBanner = await imgbbUploader(imgbbOptions).then(async (response) => {
+        return response.url
+      }).catch((error) => console.error(error))
+    }
+
+    data.profilePictureSrc = newProfilePicture
+    data.bannerImageSrc = newBanner
+
+    return userCollection.findOneAndUpdate({ "_id": id }, { $set: data })
+
   } catch (error) {
     console.log("Error in userService.update: ", error);
   }
 };
+
 userService.delete = async (id) => {
   try {
     if (!id) throw new Error("Missing id");
